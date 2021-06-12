04ecedf121e330af4108f45c561c0c0802b25e20
Fenomen-Alex <alexfenomen@gmail.com>
2021-06-12 23:14:07 +0300

    Fixing login

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,10 +3,21 @@ import {Route} from 'react-router-dom';
 import {useAuth0} from "@auth0/auth0-react";
 
 const PrivateRoute = ({children, ...rest}) => {
-  const { loginWithRedirect, isAuthenticated, user} = useAuth0();
+  const { loginWithRedirect, handleRedirectCallback, isAuthenticated, user} = useAuth0();
 
   if (!isAuthenticated && !user) {
     return loginWithRedirect();
+  }
+
+  if (
+    window.location.search.includes('code=') &&
+    window.location.search.includes('state=')
+  ) {
+    try {
+      handleRedirectCallback();
+    } catch (err) {
+      alert(err);
+    }
   }
 
   return (
