4e1f6b8b206e6744506d0fd733b8a911754ee5f5
Fenomen-Alex <alexfenomen@gmail.com>
2021-06-12 22:57:20 +0300

    Fixing router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import SiteHeader from './components/SiteHeader';
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
